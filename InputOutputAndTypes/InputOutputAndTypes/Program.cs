77c9a137176d739e94067a0d2f02d561fbf5a190
DimaL45600 <dima45600amid@gmail.com>
2024-09-07 17:52:34 +0300

    Добавлен код, презентации и домашнее задание первого занятия

---
+++ InputOutputAndTypes/InputOutputAndTypes/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace InputOutputAndTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создание и инициализация переменных
+            var name = "Danil";
+            int a1 = 5;
+            int a2 = 0;
+            byte b1 = 65;
+            int b2 = 50;
+            //Блок try 
+            try
+            {
+                Console.WriteLine($"B1 = {b1}");
+                Console.WriteLine($"--B1 = {--b1}");
+                Console.WriteLine($"B1 = {b1}");
+                Console.WriteLine($"B1-- = {b1--}");
+                Console.WriteLine($"B1 = {b1}");
+                Console.WriteLine($"{b1} + {b2} = {b1 += (byte)b2}");
+                b2 = b1;
+                Console.WriteLine($"b2 = {b2}");
+                b2 = 10000;
+                checked { b1 = (byte)b2; };
+                Console.WriteLine($"b1 = {b1}");
+                Console.WriteLine($"{a1 / a2}");
+            }
+            //Блок catch при делении на ноль
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine ("Zero divide");
+            }
+            //Блок catch при остальных случаях
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            //ввод целого числа
+            var age = Convert.ToInt32(Console.ReadLine());
+            //простой вывод
+            Console.WriteLine("Hello, World!");
+            //вывод с переменными и форматирование
+            Console.WriteLine($"{name}(age - {age.ToString("+####;-###;Zero")}), is human!");
+        }
+    }
+}
