0452456bfdbf7746374dfc775919a6cbab67bf34
DimaL45600 <dima45600amid@gmail.com>
2024-11-17 14:49:37 +0200

    Добавлен код занятия и домашняя работа

---
+++ Lessons Code/Algorytms/Algorytms/Program.cs
@@ -0,0 +1,341 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Algorytms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stopwatch sw = new Stopwatch();
+            //int[] original = GenerateRandomArray(1000, 1, 100);
+            //int[] array = original.ToArray();
+
+            //Console.WriteLine(String.Join(", ", array));
+
+            //int iter = 0;
+            //int move = 0;
+            //bool sorted = true;
+
+            //iter = 0;
+            //move = 0;
+            //sw.Restart();
+            //sw.Start();
+            //do
+            //{
+            //    sorted = true;
+            //    iter++;
+            //    for (int i = 0; i < array.Length - 1; i++)
+            //    {
+            //        if (array[i] > array[i + 1])
+            //        {
+            //            //Console.WriteLine($"{array[i]} < {array[i+1]}");
+            //            int temp = array[i];
+            //            array[i] = array[i + 1];
+            //            array[i + 1] = temp;
+            //            sorted = false;
+            //            move++;
+            //        }
+            //    }
+            //} while (!sorted);
+            //sw.Stop();
+            //Console.WriteLine(String.Join(", ", array));
+            //Console.WriteLine($"Iteration = {iter}; Move = {move}; Time = {sw.ElapsedTicks.ToString()}");
+
+            //array = original.ToArray();
+            //iter = 0;
+            //move = 0;
+            //sw.Restart();
+            //sw.Start();
+            //do
+            //{
+            //    sorted = true;
+            //    iter++;
+            //    for (int i = 0; i < array.Length-1; i++)
+            //    {
+            //        if (array[i] > array[i + 1])
+            //        {
+            //            //Console.WriteLine($"{array[i]} < {array[i+1]}");
+            //            int temp = array[i];
+            //            array[i] = array[i + 1];
+            //            array[i + 1] = temp;
+            //            sorted = false;
+            //            move++;
+            //        }
+            //    }
+            //    for (int i = array.Length - 1; i > 0; i--)
+            //    {
+            //        if (array[i] < array[i - 1])
+            //        {
+            //            //Console.WriteLine($"{array[i]} < {array[i+1]}");
+            //            int temp = array[i];
+            //            array[i] = array[i - 1];
+            //            array[i - 1] = temp;
+            //            sorted = false;
+            //            move++;
+            //        }
+            //    }
+            //} while (!sorted);
+            //sw.Stop();
+            //Console.WriteLine(String.Join(", ", array));
+            //Console.WriteLine($"Iteration = {iter}; Move = {move}; Time = {sw.ElapsedTicks.ToString()}");
+
+            //sw.Start();
+            //array = original.ToArray();
+            //QuickSort(array, 0, array.Length - 1);
+            //sw.Stop();
+            //Console.WriteLine(String.Join(", ", array));
+            //Console.WriteLine($"Time = {sw.ElapsedTicks.ToString()}");
+
+            //sw.Start();
+            //array = original.ToArray();
+            //InsertionSort(array, out iter, out move);
+            //sw.Stop();
+            //Console.WriteLine(String.Join(", ", array));
+            //Console.WriteLine($"Iteration = {iter}; Move = {move}; Time = {sw.ElapsedTicks.ToString()}");
+
+
+            //void InsertionSort(int[] array, out int iter, out int move )
+            //{
+            //    iter = 0;
+            //    move = 0;
+            //    int n  = array.Length;
+            //    for (int i = 1; i < n; i++)
+            //    {
+            //        iter++;
+            //        int key = array[i];
+            //        int j = i - 1;
+            //        while (j >= 0 && array[j] > key)
+            //        {
+            //            move++;
+            //            array[j+1] = array[j];
+            //            j--;
+            //        }
+            //        array[j+1] = key;
+            //    }
+            //}
+
+
+
+
+            //void QuickSort(int[] array, int low, int high)
+            //{
+            //    if(low < high)
+            //    {
+            //        int pivotIndex = Partition(array, low, high);
+            //        QuickSort(array, low, pivotIndex-1);
+            //        QuickSort(array, pivotIndex + 1, high);
+            //    }
+            //}
+
+            //int Partition(int[] array, int low, int high)
+            //{
+            //    int pivot = array[high];
+            //    int i = low - 1;
+
+            //    for(int j = low; j < high; j++)
+            //    {
+            //        if(array[j] < pivot)
+            //        {
+            //            i++;
+            //            int temp = array[i];
+            //            array[i] = array[j];
+            //            array[j] = temp;
+            //        }
+            //    }
+
+            //    int swap = array[i + 1];
+            //    array[i + 1] = array[high];
+            //    array[high] = swap;
+            //    return i + 1;
+            //}
+
+            //Graph graph = new Graph();
+            //graph.addVertex(1);
+            //graph.addVertex(2);
+            //graph.addVertex(3);
+            //graph.addVertex(4);
+            //graph.addVertex(5);
+            //graph.AddEdge(1, 2);
+            //graph.AddEdge(1, 3);
+            //graph.AddEdge(2, 4);
+            //graph.AddEdge(4, 3);
+            //graph.AddEdge(4, 5);
+            //graph.Display();
+            //graph.DFS(4);
+            //Console.WriteLine("--------------------");
+            //graph.BFS(4);
+
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Insert(10);
+            binaryTree.Insert(8);
+            binaryTree.Insert(6);
+            binaryTree.Insert(9);
+            binaryTree.Insert(12);
+            binaryTree.Insert(10);
+            Console.WriteLine("--------------------");
+            binaryTree.PreOrder(binaryTree.Root);
+            Console.WriteLine("--------------------");
+            binaryTree.InOrder(binaryTree.Root);
+            Console.WriteLine("--------------------");
+            binaryTree.PostOrder(binaryTree.Root);
+        }
+        public static int[] GenerateRandomArray(int size, int min, int max)
+        {
+            Random rnd = new Random();
+            int[] array = new int[size];
+
+            for(int i = 0; i<size; i++)
+            {
+                array[i] = rnd.Next(min, max);
+            }
+
+            return array;
+        }
+
+    }
+
+
+    public class TreeNode
+    {
+        public int value;
+        public TreeNode? left;
+        public TreeNode? right;
+
+        public TreeNode(int value)
+        {
+            this.value = value;
+            left = right = null;
+        }
+    }
+
+    public class BinaryTree
+    {
+        public TreeNode Root;
+
+        public void Insert(int value)
+        {
+            Root = InsertRecursive(Root, value);
+        }
+
+        public TreeNode InsertRecursive(TreeNode root, int value)
+        {
+            if (root == null)
+            {
+                return new TreeNode(value);
+            }
+            if(value < root.value)
+            {
+                root.left = InsertRecursive(root.left, value);
+            }else if (value > root.value)
+            {
+                root.right = InsertRecursive(root.right, value);
+            }
+
+            return root;
+        }
+
+        public void PreOrder(TreeNode node)
+        {
+            if (node == null) return;
+
+            Console.WriteLine(node.value);
+            PreOrder(node.left);
+            PreOrder(node.right);
+        }
+
+        public void InOrder(TreeNode node)
+        {
+            if (node == null) return;
+
+            InOrder(node.left);
+            Console.WriteLine(node.value);
+            InOrder(node.right);
+        }
+
+        public void PostOrder(TreeNode node)
+        {
+            if (node == null) return;
+
+            PostOrder(node.left);
+            PostOrder(node.right);
+            Console.WriteLine(node.value);
+        }
+    }
+
+    public class Graph
+    {
+        private Dictionary<int, List<int>> adjList = new Dictionary<int, List<int>>();
+
+        public void addVertex(int vertex)
+        {
+            if(!adjList.ContainsKey(vertex))
+            {
+                adjList[vertex] = new List<int>();
+            }
+        }
+        public void AddEdge(int from, int to)
+        {
+            if(adjList.ContainsKey(from) && adjList.ContainsKey(to))
+            {
+                adjList[from].Add(to);
+                adjList[to].Add(from);
+            }
+        }
+        public void Display()
+        {
+            foreach(var vertex in adjList)
+            {
+                Console.Write($"{vertex.Key}: ");
+                foreach(var neighbor in vertex.Value)
+                {
+                    Console.Write($"{neighbor} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void DFS(int start)
+        {
+            var visited = new HashSet<int>();
+            DFSRecursive(start, visited);
+        }
+        private void DFSRecursive(int vertex, HashSet<int> visited)
+        {
+            if (!visited.Contains(vertex))
+            {
+                Console.WriteLine(vertex.ToString());
+                visited.Add(vertex);
+                foreach (var neighbor in adjList[vertex])
+                {
+                    DFSRecursive(neighbor, visited);
+                }
+            }
+        }
+
+        public void BFS(int start)
+        {
+            var visted = new HashSet<int>();
+            var queue = new Queue<int>();
+
+            queue.Enqueue(start);
+            visted.Add(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                Console.WriteLine(vertex);
+
+                foreach(var nighbor in adjList[vertex])
+                {
+                    if (!visted.Contains(nighbor))
+                    {
+                        queue.Enqueue(nighbor);
+                        visted.Add(nighbor);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Lessons Code/AsyncBaseConsole/AsyncBaseConsole/Program.cs
@@ -0,0 +1,154 @@
+﻿namespace AsyncBaseConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread curentTread = Thread.CurrentThread;
+
+            //Console.WriteLine($"Name: {curentTread.Name}");
+            //Console.WriteLine($"{curentTread.IsAlive}");
+            //Console.WriteLine($"{curentTread.ManagedThreadId}");
+            //Console.WriteLine($"{curentTread.Priority}");
+            //Console.WriteLine($"{curentTread.ThreadState}");
+
+
+
+            //void Print() => Console.WriteLine("Hello thread");
+
+            //Thread myTread1 = new Thread(Print);
+            //Thread myTread2 = new Thread(new ThreadStart(Print));
+            //Thread myTread3 = new Thread(() => Console.WriteLine("Hello thread"));
+
+            //myTread1.Start();
+            //myTread2.Start();
+            //myTread3.Start();
+
+            //void Print()
+            //{
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        Console.WriteLine($"Second tread: {i}");
+            //        Thread.Sleep(400);
+            //    }
+            //}
+            //Thread myThread = new Thread(Print);
+            //myThread.Start();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine($"Main tread: {i}");
+            //    Thread.Sleep(600);
+            //}
+
+
+            //void Print(object? message) => Console.WriteLine(message);
+
+            //Thread myTread1 = new Thread(Print);
+            //Thread myTread2 = new Thread(new ParameterizedThreadStart(Print));
+            //Thread myTread3 = new Thread(message => Console.WriteLine(message));
+
+            //myTread1.Start("Hello");
+            //myTread2.Start("Privet");
+            //myTread3.Start("Hola");
+
+
+            //void Print(object? obj)
+            //{
+            //    if(obj is Person person) Console.WriteLine(person.Name);
+            //}
+            //int number = 9;
+            //Person p = new Person("Jack", 45);
+            //Thread myThread = new Thread(p.Print);
+            //myThread.Start();
+
+            //int x = 0;
+            //object locker = new object();
+
+            //for(int i = 1; i < 6; i++)
+            //{
+            //    Thread myThread = new Thread(Print);
+            //    myThread.Name = $"Thread {i}";
+            //    myThread.Start();
+            //}
+            //void Print()
+            //{
+            //    lock (locker)
+            //    {
+            //        x = 1;
+            //        for (int i = 1; i < 6; i++)
+            //        {
+            //            Console.WriteLine($"{Thread.CurrentThread.Name}: {x}");
+            //            x++;
+            //            Thread.Sleep(100);
+            //        } 
+            //    }
+            //}
+
+
+            //Console.WriteLine("main starts");
+            //Task task = new Task(() =>
+            //{
+            //    Console.WriteLine("Task starts");
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Task ends");
+            //});
+            //Console.WriteLine($"{task.Status}");
+            //task.Start();
+            //Console.WriteLine($"{task.Id}");
+            //Console.WriteLine($"{task.IsCompleted}");
+            //Console.WriteLine($"{task.Status}");
+            //task.Wait();
+            ////task.RunSynchronously();
+            //Console.WriteLine("main ends");
+
+            //var outer = Task.Factory.StartNew(() =>      // внешняя задача
+            //{
+            //    Console.WriteLine("Outer task starting...");
+            //    var inner = Task.Factory.StartNew(() =>  // вложенная задача
+            //    {
+            //        Console.WriteLine("Inner task starting...");
+            //        Thread.Sleep(2000);
+            //        Console.WriteLine("Inner task finished.");
+            //    }, TaskCreationOptions.AttachedToParent);
+            //});
+            //outer.Wait(); // ожидаем выполнения внешней задачи
+            //Console.WriteLine("End of Main");
+
+            //Task[] tasks1 = new Task[3]
+            //{
+            //    new Task(() => Console.WriteLine("First Task")),
+            //    new Task(() => Console.WriteLine("Second Task")),
+            //    new Task(() => Console.WriteLine("Third Task"))
+            //};
+            //foreach (var task in tasks1)
+            //{
+            //    task.Start();
+            //}
+
+
+            //Task[] tasks2 = new Task[3];
+            //int j = 1;
+            //for (int i = 0; i < tasks2.Length; i++)
+            //    tasks2[i] = Task.Factory.StartNew(() => Console.WriteLine($"Task {j++}"));
+            //Task.WaitAny(tasks2);
+
+            int n1 = 4, n2 = 10;
+            int Sum(int a, int b) => a + b;
+            Task<int> sumTask = new Task<int>(() => Sum(n1, n2));
+            sumTask.Start();
+
+            int result = sumTask.Result;
+            Console.WriteLine($"{n1} + {n2} = {result}");
+
+        }
+    }
+    record class Person(string Name, int Age)
+    {
+        public void Print()
+        {
+            Console.WriteLine($"Name = {Name}");
+            Console.WriteLine($"Age = {Age}");
+        }
+    }
+}
